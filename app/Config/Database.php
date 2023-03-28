39334e26eada1f7a24fa738fdcd9a407d5ae57e4
Abdullah10291919 <HDmodz4you@gmail.com>
2023-03-28 12:15:48 +0100

    modification in database file

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'database'    => 'movies',
+        'username'    => '2109134',
+        'password'    => 'p73ux2',
+        'database'    => 'db2109134',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
